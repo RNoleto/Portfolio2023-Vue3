19134c966dd8a1cfb76b719dc06e1279a48df423
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-07-07 23:22:43 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <PrimeirComponente/>
+</template>
+
+<script>
+import PrimeirComponente from './components/PrimeirComponente.vue';
+
+
+export default{
+  name: 'App',
+  components: {
+    PrimeirComponente
+  }
+}
+</script>
+
+<style lang="scss">
+@import './global.scss';
+</style>
