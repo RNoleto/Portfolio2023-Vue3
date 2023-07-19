7c559c9d23ef2d3667ba78d1128f008641853b77
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-07-18 23:11:09 -0300

    instalação de pacote de icones e inicio da estrutura da home mais navbar

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,23 @@
 <template>
-  <div>
-    <h1>Pagina Home</h1>
+  <section>
+    <div class="container">
+      <div class="introduction">
+        <h3>Oi, eu sou Ramon</h3>
+        <h2>VUE</h2>
+        <h2>Developer<span>.</span></h2>
+      </div>
   </div>
+  </section>
 </template>
+
+<script></script>
+
+<style lang="scss" scoped>
+.container{
+  padding: 1rem;
+  .introduction{
+    width: max-content;
+    background-color: blue;
+  }
+}
+</style>
