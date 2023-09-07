3a4c90f1842f98bdcb1e1e883c90760a881b9cf5
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-09-07 01:35:23 -0300

    iniciando banner na home

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="header">
     <div class="container">
-      <router-link :to="{name: 'home'}">Noleto</router-link>
+      <router-link :to="{name: 'home'}"><span>Noleto.</span></router-link>
       <nav class="menu">
         <router-link :to="{name: 'home'}"><p>Menu 1</p></router-link>
         <router-link :to="{name: 'home'}"><p>Menu 2</p></router-link>
@@ -20,17 +20,20 @@ export default{
   width: 100%;
   padding: 1.5rem 0;
   border-bottom: 1px solid #999;
-  border-radius: 0 0 15px 15px;
   box-shadow: 3px 1px 3px rgba(0,0,0, .3);
   .container{
     display: flex;
     justify-content: space-between;
+    span{
+      font-size: 22px;
+    }
     .menu{
       display: flex;
       gap: 1rem;
       cursor: pointer;
+      font-size: 16px;
       p:hover{
-        color: blue;
+        color: var(--color2);
       }
     }
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,12 @@
 <template>
-  
+  <div class="container">
+    <div class="banner">
+      <div class="text">
+        <h2>Olá!</h2>
+        <h1>Sou Desenvolvedor Web.</h1>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,5 +15,7 @@ export default{
 </script>
 
 <style lang="scss" scoped>
-
+.text{
+  padding: 5rem 0px;
+}
 </style>
