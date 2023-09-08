0ebafd3ed3112949893fcd0050e45d7baded73b6
Ramon Ribeiro Noleto <ramon.ribeiro@inbraep.com.br>
2023-09-08 08:37:35 -0300

    ajustes informação da home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,13 @@
       <div class="text">
         <h2>Olá!</h2>
         <h1>Sou Desenvolvedor Web.</h1>
+        <p>Me chamo Ramon Noleto e trabalho com desenvolvimento web a 1 ano.</p>
+        <div class="buttons">
+          <a href="" class="btn">Botão</a>
+        </div>
+      </div>
+      <div class="image">
+        <h2>Espaço para imagem</h2>
       </div>
     </div>
   </div>
@@ -15,7 +22,16 @@ export default{
 </script>
 
 <style lang="scss" scoped>
-.text{
-  padding: 5rem 0px;
+.banner{
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 10rem 1rem;
+  background-color: pink;
+  .text{
+  .buttons{
+    margin-top: 10px;
+  }
+}
 }
 </style>
